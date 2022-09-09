b403555555e132a616efbd0a520506ade8d5fda0
zeyuzhao12 <98190274+zeyuzhao12@users.noreply.github.com>
2022-09-09 02:56:22 -0400

    Add files via upload

---
+++ inclassR1.R
@@ -0,0 +1,12 @@
+  install.packages("MASS")
+  library(MASS)
+  attach(Boston)
+  ?Boston
+  head(Boston)
+  dim(Boston)
+  nrow(Boston)
+  ncol(Boston)
+  names(Boston)
+  str(Boston)
+  summary(Boston)
+  
